2c3b8a1144f55624dbee97bbe370f0099292b84c
Anu Slorah <anuslorah@hotmail.com>
2019-01-17 10:08:42 -0800

    Add files via upload
    working html and css for the form

---
+++ php/form.php
@@ -0,0 +1,8 @@
+/** The form data entered in the html should be sent via GET method. 
+*Your PHP script should echo the data back to the user in the following format:
+*
+*Name : User entered data>
+*Email: User entered data>
+*Phone : User entered data, if no user entered data, do not show this line>
+*State: User entered data, if no user entered data, do not show this line>
+**/
