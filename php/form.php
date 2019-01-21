19dce0ab3c315252848b219fa22ae79bbbc4034c
Anu Slorah <anuslorah@hotmail.com>
2019-01-21 14:14:03 -0800

    Add files via upload
    Added php to strip form data from extra characters and display in a nice little div

--- php/form.php
+++ php/form.php
@@ -1,8 +1,35 @@
-/** The form data entered in the html should be sent via GET method. 
-*Your PHP script should echo the data back to the user in the following format:
-*
-*Name : User entered data>
-*Email: User entered data>
-*Phone : User entered data, if no user entered data, do not show this line>
-*State: User entered data, if no user entered data, do not show this line>
-**/
+<?php
+
+//clean strings user has enteres
+function cleanString($string) {
+	//remove HTML & script tags
+	$string = strip_tags($string); 	
+	//allow alphanumerics & punctuation - convert the rest to single spaces
+	$string = preg_replace("/[^[:alnum:][:punct:]]/", " " ,$string);  
+	return $string;
+}
+
+//read data from form and spit it out in a nice div
+function spitOutData() {
+	if ($_SERVER['REQUEST_METHOD'] === "GET") {
+		$name = cleanString($_GET['name']); 
+		$email = cleanString($_GET['email']);
+		$phonenumber = cleanString($_GET['phonenumber']);
+		$state = $_GET['state'];
+
+		echo "<div style='width:60%; 
+			margin: 0 auto; 
+			margin-top: 40px;
+			background-color: aliceblue;
+			padding: 20px;'>";
+		echo "Name: " . $name . "<br/>" . "<br/>" 
+			. "Email: " . $email . "<br/>" . "<br/>"
+			. "Phone number: " . $phonenumber . "<br/>" . "<br/>"
+			. "State: " . $state;
+		echo "</div>";
+	}
+}	
+
+spitOutData();
+
+?>              
